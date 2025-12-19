ba492ed67a1a22e3f33659af17e3df3eb1e0e490
Vaddex <Gosvlad236@gmail.com>
2025-12-19 03:55:48 +0200

    added main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import MainPage from "./components/MainPage/MainPage";
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <MainPage />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,10 @@
+function MainPage() {
+    return (
+        <div>
+            <h1>Welcome to the Main Page</h1>
+            <p>This is the main content of the application.</p>
+        </div>
+    );
+}
+
+export default MainPage;
