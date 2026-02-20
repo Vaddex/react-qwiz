31be5d939858452a1d8842b2b0538d5a16cee545
Vaddex <89461632+Vaddex@users.noreply.github.com>
2026-02-20 22:26:58 +0200

    added MainPage styles

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -2,8 +2,14 @@ import css from "./MainPage.module.css";
 function MainPage() {
     return (
         <div className={css["container"]}>
-            <h1>Welcome to the Main Page</h1>
-            <p>This is the main content of the application.</p>
+            <h1 className={css["topSign"]}>Welcome to the Main Page</h1>
+            <p className={css["description"]}>
+                [test text] Quick, free film quiz to challenge your knowledge.
+                Instant results. Lorem ipsum dolor sit amet consectetur,
+                adipisicing elit. Ipsam consectetur modi unde optio culpa.
+                Voluptate beatae delectus iusto. Id eaque et molestias ad
+                veniam. Eius labore illum explicabo minus neque. [test text]
+            </p>
         </div>
     );
 }
