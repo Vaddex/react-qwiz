540d2cf183da9975ef177be065e10b086c766d6e
Vaddex <89461632+Vaddex@users.noreply.github.com>
2026-02-20 22:02:20 +0200

    added style to MainPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 import CardsField from "./components/CardsField/CardsField";
 import MainPage from "./components/MainPage/MainPage";
 
 function App() {
     return (
         <>
+            <Header />
             <MainPage />
             <CardsField />
         </>
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,6 +1,7 @@
+import css from "./MainPage.module.css";
 function MainPage() {
     return (
-        <div>
+        <div className={css["container"]}>
             <h1>Welcome to the Main Page</h1>
             <p>This is the main content of the application.</p>
         </div>
