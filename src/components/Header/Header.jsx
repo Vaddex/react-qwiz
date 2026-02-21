42e396f3d997bee76057e87cad59f60ac1ac66ce
Vaddex <89461632+Vaddex@users.noreply.github.com>
2026-02-21 22:52:05 +0200

    added Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
+import css from "./Header.module.css";
+
 function Header() {
     return (
-        <header>
-            <h1>Qwiz</h1>
+        <header className={css["header"]}>
+            <div className={css["headerLeft"]}>
+                <button className={css["headerBtn"]}>Blitz Quiz</button>
+                <button className={css["headerBtn"]}>Another button</button>
+            </div>
+
+            <h1 className={css["headerTitle"]}>Qwiz</h1>
+
+            <div className={css["headerRight"]}>
+                <button className={css["headerBtn"]}>Login</button>
+            </div>
         </header>
     );
 }
